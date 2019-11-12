9edb38885b607c7a22cde4ce32241aede614f244
gojib2002 <gojib2002@naver.com>
2019-11-12 23:48:39 +0900

    Create gojib2002_12900.cpp

---
+++ 2019_11_week2/gojib2002_12900.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+
+
+int solution(int n) {
+    int dp[60001];
+    int i;
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    for(i=3;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] %= MOD;
+    }
+    
+    return dp[n];
+}
